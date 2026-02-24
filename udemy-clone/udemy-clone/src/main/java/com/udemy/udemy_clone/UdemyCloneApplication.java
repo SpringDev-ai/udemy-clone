0c075dc7434114a64fe628e6b972ce08ace41845
SpringDev-ai <palvikram73@gmail.com>
2026-02-24 10:34:39 +0530

    initial set-up

---
+++ udemy-clone/udemy-clone/src/main/java/com/udemy/udemy_clone/UdemyCloneApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.udemy_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyCloneApplication.class, args);
+	}
+
+}
---
+++ udemy-clone/udemy-clone/src/test/java/com/udemy/udemy_clone/UdemyCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.udemy_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
